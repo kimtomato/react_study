da2e7ade08dcc43194660bbb63a0e98ae70b0d7d
kimtomato <wltn5195@naver.com>
2020-08-24 02:56:03 +0900

    props

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import MyComponent from "./MyComponent";
+
+const App = () => {
+  return (
+    <MyComponent name="dmswl" favoriteNumber={9}>
+      {" "}
+      리액트{" "}
+    </MyComponent>
+  );
+};
+
+export default App;
